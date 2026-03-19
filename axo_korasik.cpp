ab1cc4c696e9780c8b299b011a7a82ab5edc0065
арсений артемов <arsen.domoded@gmail.com>
2026-03-19 15:54:19 +0300

    fixed Axo-Korasik

--- axo_korasik.cpp
+++ axo_korasik.cpp
@@ -9,10 +9,11 @@ struct node {
     bool is_leaf;
     vector<node*> next;
     vector<node*> autom_go;
-    node *prev = nullptr;
+    node* prev = nullptr;
     char prevEdge;
-    node *suf_ref = nullptr;
+    node* suf_ref = nullptr;
     bool is_root = false;
+    bool is_terminal = false;
 
 
     node() : is_leaf(true), next(26, nullptr), autom_go(26, nullptr) {};
@@ -20,10 +21,12 @@ struct node {
 
 
 
-void add_string(const string & s, node *root) {
-    node *cur = root;
-    node *prev = nullptr;
-    for (char c : s) {
+void add_string(const string& s, node* root) {
+    node* cur = root;
+    node* prev = nullptr;
+    char c;
+    for (int i = 0; i < s.size(); ++i) {
+        c = s[i];
         if (!cur->next[c - 'a']) {
             cur->next[c - 'a'] = new node;
             cur->next[c - 'a']->prev = cur;
@@ -32,16 +35,18 @@ void add_string(const string & s, node *root) {
         }
         cur = cur->next[c - 'a'];
     }
+    cur->is_terminal = true;
 }
 
-node *getlink(node *cur, char c);
+node* getlink(node* cur, char c);
 
 
-node *getSufflink(node *cur) {
-    if (!cur->prev) {
+node* getSufflink(node* cur) {
+    if (!cur->suf_ref) {
         if (cur->prev->is_root) {
             cur->suf_ref = cur->suf_ref = cur->prev;
-        } else {
+        }
+        else {
             cur->suf_ref = getlink(getSufflink(cur->prev), cur->prevEdge);
         }
     }
@@ -49,14 +54,15 @@ node *getSufflink(node *cur) {
 
 }
 
-node *getlink(node *cur, char c) {
+node* getlink(node* cur, char c) {
     if (!cur->autom_go[c - 'a']) {
         if (cur->next[c - 'a']) {
             cur->autom_go[c - 'a'] = cur->next[c - 'a'];
         }
         else if (cur->is_root) {
             cur->autom_go[c - 'a'] = cur;
-        } else {
+        }
+        else {
             cur->autom_go[c - 'a'] = getlink(getSufflink(cur), c);
         }
     }
@@ -65,22 +71,26 @@ node *getlink(node *cur, char c) {
 }
 
 
-void ParseText(string text, node *root) {
-    node *cur = root;
+void ParseText(string text, node* root) {
+    node* cur = root;
     for (char c : text) {
-        if (cur->is_leaf) {
+        if (cur->is_terminal) {
             cout << "found" << endl;
         }
         cur = getlink(cur, c);
-        
+
+    }
+
+    if (cur->is_terminal) {
+        cout << "found" << endl;
     }
 }
 
 
 
 
-ostream & operator << (ostream & out, const vector<char> & v) {
-    for (auto el :v){
+ostream& operator << (ostream& out, const vector<char>& v) {
+    for (auto el : v) {
         out << el;
     }
     out << '\n';
@@ -89,10 +99,12 @@ ostream & operator << (ostream & out, const vector<char> & v) {
 }
 
 
-void output(node *root, vector<char> &v) {
-    if (root->is_leaf) {
+void output(node* root, vector<char>& v) {
+    if (root->is_terminal) {
         cout << v;
-    } else {
+    }
+
+    if (!root->is_leaf) {
         for (int i = 0; i < 26; ++i) {
             if (root->next[i]) {
                 v.push_back('a' + i);
@@ -106,7 +118,7 @@ void output(node *root, vector<char> &v) {
 
 
 int main() {
-    node *root = new node;
+    node* root = new node;
     cout << "1\n";
     for (int i = 0; i < 6; ++i) {
         string s;
